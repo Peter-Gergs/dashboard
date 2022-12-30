c21955d4a6fc87c6d0ba87c9b18eac39dddbbdac
Peter-Gergs <80621754+Peter-Gergs@users.noreply.github.com>
2022-12-30 15:18:48 +0200

    fix-url

--- src/App.js
+++ src/App.js
@@ -19,13 +19,13 @@ function App() {
         <Header />
         <Routes>
           <Route path="*" element={<Dashboard />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="projects" element={<Projects />} />
-          <Route path="courses" element={<Courses />} />
-          <Route path="friends" element={<Friends />} />
-          <Route path="files" element={<Files />} />
-          <Route path="plans" element={<Plans />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/friends" element={<Friends />} />
+          <Route path="/files" element={<Files />} />
+          <Route path="/plans" element={<Plans />} />
         </Routes>
       </div>
     </div>
